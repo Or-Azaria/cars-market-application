f8d1608d50fc2210d1b7ec8a37367b67878ab59c
Orazrya <96197345+Orazrya@users.noreply.github.com>
2023-07-25 21:00:41 +0300

    Adding first commit next 13

---
+++ components/Hero.tsx
@@ -0,0 +1,15 @@
+import Image from 'next/image';
+
+import React from 'react';
+
+const Hero = () => {
+  return (
+    <div className="hero">
+      <div className="flex-1 pt-36 padding-x">
+        <h1 className="hero_title"></h1>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
