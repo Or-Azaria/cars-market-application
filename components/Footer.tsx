c9ce362fc57705134dd4cf6bd52c44213fa1fdb0
Orazrya <96197345+Orazrya@users.noreply.github.com>
2023-07-27 16:13:10 +0300

    Editing the car image

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,6 @@
+import { footerLinks } from '@/constants';
 import Image from 'next/image';
 import Link from 'next/link';
-
 
 const Footer = () => {
   return (
@@ -23,7 +23,31 @@ const Footer = () => {
           </p>
         </div>
         <div className="footer__links">
-
+          {footerLinks.map((link) => (
+            <div key={link.title} className="footer__link">
+              <h3 className="font-bold">{link.title}</h3>
+              {link.links.map((item) => (
+                <Link
+                  key={item.title}
+                  href={item.url}
+                  className="text-gray-500"
+                >
+                  {item.title}
+                </Link>
+              ))}
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="flex justify-between items-center flex-wrap mt-10 border-t border-gray-500 sm:px-6 py-6 ">
+        <p>@2023 Carhub. All rights Reserved</p>
+        <div className="footer__copyrights-link">
+          <Link href="/" className="text-gray-500">
+            Privacy Policy
+          </Link>
+          <Link href="/" className="text-gray-500">
+            Terms of Use
+          </Link>
         </div>
       </div>
     </footer>
